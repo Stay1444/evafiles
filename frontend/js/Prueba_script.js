c95b759ef2e86e57951e0a2a85c916d33fb59b20
ElCarpi <carlos.pijuangarcia@gmail.com>
2023-03-27 11:31:05 +0200

    Botón interactivo listo

--- frontend/js/Prueba_script.js
+++ frontend/js/Prueba_script.js
@@ -1,7 +1,7 @@
 document.getElementById("search_button").style.display = "none";
 
 document.getElementById("search_bar").addEventListener("keydown", (e)=> {
-    if (e.target.value=="")
+    if (e.target.value.trim ()=="")
     {
         document.getElementById("upload_button").style.display = "inline";
         document.getElementById("search_button").style.display = "none";
@@ -12,5 +12,17 @@ document.getElementById("search_bar").addEventListener("keydown", (e)=> {
     }
     e.getElementById("search_button").value="buscar"
 console.log("funciona")
+});
 
-})
+document.getElementById("search_bar").addEventListener("input", (e)=> {
+if (e.target.value.trim ()!="")
+{
+    document.getElementById("upload_button").style.display = "none";
+    document.getElementById("search_button").style.display = "inline";
+} else  {
+    document.getElementById("upload_button").style.display = "inline";
+    document.getElementById("search_button").style.display = "none";
+}
+e.getElementById("search_button").value="subir"
+
+});
