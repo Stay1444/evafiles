7ecff0230a22449ee6e70fe70b4b89fed3b09f1b
ElCarpi <carlos.pijuangarcia@gmail.com>
2023-03-27 10:05:47 +0200

    js interacción botones

---
+++ frontend/js/Prueba_script.js
@@ -0,0 +1,14 @@
+document.getElementById("search_bar").addEventListener("change", (e)=> {
+    if (e.target.value=="")
+    {
+        document.getElementById("upload_button").style.display = "inline";
+        document.getElementById("upload_search_button").style.display = "none";
+    }
+    else {
+        document.getElementById("upload_button").style.display = "none";
+        document.getElementById("upload_search_button".syle.display = "inline")
+    }
+    e.getElementById("upload_search_button").value="buscar"
+console.log("funciona")
+
+})
