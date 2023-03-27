f0e37e4bffe71b1f22dfb61779181b39c30e18d3
ElCarpi <carlos.pijuangarcia@gmail.com>
2023-03-27 10:17:45 +0200

    Actualización de lo anterior

--- frontend/js/Prueba_script.js
+++ frontend/js/Prueba_script.js
@@ -1,3 +1,5 @@
+document.getElementById("upload_search_button").style.display = "none";
+
 document.getElementById("search_bar").addEventListener("change", (e)=> {
     if (e.target.value=="")
     {
@@ -6,7 +8,7 @@ document.getElementById("search_bar").addEventListener("change", (e)=> {
     }
     else {
         document.getElementById("upload_button").style.display = "none";
-        document.getElementById("upload_search_button".syle.display = "inline")
+        document.getElementById("upload_search_button".syle.display = "inline");
     }
     e.getElementById("upload_search_button").value="buscar"
 console.log("funciona")
