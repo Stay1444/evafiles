48d299722b76f4f25134d13ffbea81ca7e4a67cd
Stay1444 <quiquelluc@gmail.com>
2023-03-28 12:56:37 +0200

    Cambios epicos

--- frontend/js/json.js
+++ frontend/js/json.js
@@ -1,17 +1,29 @@
-var name = document.getElementById("filename")
-var description = document.getElementById("description")
-var custom_url =document.getElementById("custom_url")
-var available_time=document.getElementById("available_time")
+const name = document.getElementById("filename")
+const description = document.getElementById("description")
+const available_time=document.getElementById("available_time")
+const fileInput = document.getElementById("myFile");
+
 var jsonbtn= document.getElementById("jsonbtn")
 
 jsonbtn.onclick = () => {
-    var data = {
-        name: filename.value,
-        description:description.value,
-        custom_url:custom_url.value,
-        available_time:available_time.value,
+    if (fileInput.files.length < 1) alert("Select atleast one file!");
 
-    }
-    console.log(JSON.stringify(data))
-    console.log(available_time)
+    const formData  = new FormData();
+    
+    formData.append('name', name.value);
+    formData.append('description', description.value);
+    formData.append('duration', duration.value);
+
+    //const fileData = new FileReader().readAsArrayBuffer(fileInput.files[0]);
+
+//    formData.append('file', fileData);
+
+    formData.append('file', fileInput.files[0]);
+
+    fetch("http://127.0.0.1:5000/file/upload", {
+        method: "POST",
+        body: formData
+    }).then(x => {
+        console.log(x);
+    });
 }
